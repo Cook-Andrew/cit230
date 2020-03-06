17f270b7d039bc9383ac7b09eb19e7b243fc04ca
Andrew Cook <endercook@byui.edu>
2020-03-05 20:56:27 -0700

    prophets style

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,12 +10,21 @@ fetch(requestURL)
         for (let i = 0; i < prophets.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
+            let bDate = document.createElement('h3');//added this
+            let bPlace = document.createElement('h4');//added this
+            let image = document.createElement('img');//added this
 
             h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            bDate.textContent = "Date of Birth: " + prophets[i].birthdate;//added this
+            bPlace.textContent = "Place of Birth: " + prophets[i].birthplace;//added this
 
             card.appendChild(h2);
+            card.appendChild(bDate);//added this
+            card.appendChild(bPlace);//added this
+            card.appendChild(image);//added this
 
             document.querySelector('div.cards').appendChild(card);
             image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', h2.textContent + " - " + (i + 1))//added this
         }
     });
