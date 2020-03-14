46e7612489afc78581b8d6bbcf6b3b320ceda9ce
Andrew Cook <endercook@byui.edu>
2020-03-14 03:46:31 -0600

    preston-api update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,8 +1,15 @@
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=d121dd1ca6a86fe9f63a1745820a464b";
+//api data: https://openweathermap.org/current#current_JSON
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
+        document.getElementById('current').textContent = jsObject.weather[0].main;
+        document.getElementById('temp').textContent = jsObject.main.temp;
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('windSpeed').textContent = jsObject.wind.speed;
+
+        //the following is provided by the example api-test.html. It is ledt in so that page still functions
         document.getElementById('current-temp').textContent = jsObject.main.temp;
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
         const desc = jsObject.weather[0].description; // note how we reference the weather array
