78be9180347345eb460c7ed93325483ca5f81871
Andrew Cook <endercook@byui.edu>
2020-03-26 23:05:48 -0600

    bookends

--- rafting/js/hamburger.js
+++ rafting/js/hamburger.js
@@ -2,6 +2,27 @@ $("document").ready(function(){
     $("#hamburger").click(function(){
         $("#menu").slideToggle();
     });
+    $("#logo").click(function(){
+        window.location.href = 'index.html';
+    });
+    $("#awesome").click(function(){
+        window.location.href = 'adventure_one.html';
+    });
+    $("#epic").click(function(){
+        window.location.href = 'adventure_two.html';
+    });
+    $("#supreme").click(function(){
+        window.location.href = 'adventure_three.html';
+    });
+    $("#contact").click(function(){
+        window.location.href = 'contact.html';
+    });
+    $("#registration").click(function(){
+        window.location.href = 'registration.html';
+    });
+    $("#book").click(function(){
+        window.location.href = 'registration.html';
+    });
 });
   
 var mQuery = window.matchMedia("(max-width: 42em)")
